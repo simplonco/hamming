0bfccd30b096c83aad71ff24dfd4bfd82b75956f
Adam Sakhr <adam.sakhr@gmail.com>
2016-04-21 15:50:49 +0200

    Third Check

--- hamming.js
+++ hamming.js
@@ -1,14 +1,13 @@
 function Hamming() {}
 
-Hamming.prototype.compute = function (strand1, strand2) {
-  var diff = 0;
-      for (i = 0; i < strand1.length; i++) {
+Hamming.prototype.compute = function(strand1, strand2) {
+    var diff = 0;
+    for (i = 0; i < strand1.length; i++) {
         if (strand1[i] != strand2[i]) {
-          diff = diff + 1;
+            diff = diff + 1;
         }
-      }
-      return diff;
     }
+    return diff;
 };
 
 module.exports = Hamming;
