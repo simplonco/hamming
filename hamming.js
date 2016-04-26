74e90d3c1bec902c12035a4ad58eb0cb6351e978
adora84 <abdulkader.kalaaji@gmail.com>
2016-04-26 12:00:42 +0200

    adora solution5

--- hamming.js
+++ hamming.js
@@ -1,21 +1,13 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
-
-
-function Hamming() {};
+function Hamming() {}
 //finally i did it
 Hamming.prototype.compute = function(strand1, strand2) {
-  +var dif = 0;
-   +var len = strand1.length;
-   +for (var i=0; i<= len ; i++){
-   +  if (strand1.substr(i, 1)!==(strand2.substr(i, 1))) {
-   +    dif= dif +1;
-   +  }
-   +}
-   +return dif;
-    };
+    var dif = 0;
+    for (var i = 0; i <= strand1.length; i++) {
+        if (strand1[i] != strand2[i]) {
+            dif = dif + 1;
+        }
+    }
+    return dif;
+};
 
-    module.exports = Hamming;
+module.exports = Hamming;
